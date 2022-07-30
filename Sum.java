c7df549f09f9a68b1a3dd931809850c6badcf3e2
ShivaG523 <guptashiva058@gmail.com>
2022-07-30 13:40:38 +0530

    aded message [D[D[D[D[D[D[D[D[D[D[D[D[D[Cd[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[Dded message in Sum.java

--- Sum.java
+++ Sum.java
@@ -9,7 +9,7 @@ class Main {
     System.out.println(first + " " + second);
 
     // add two numbers
-    int sum = first + second;
-    System.out.println("The sum is: " + sum);
+    int s = first + second;
+    System.out.println("The sum is: " + s);
   }
 }
